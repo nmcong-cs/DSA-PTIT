cff852cd7b46d65c159d5e9265a19e5d085940a2
nmcong-cs <nguyenmanhcongzzz@gmail.com>
2025-02-25 15:57:45 +0700

    Update DSA6002.cpp

--- DSA6002.cpp
+++ DSA6002.cpp
@@ -12,7 +12,7 @@ int main(){
         cin >> n >> k;
         int a[n];
         for(int i=0;i<n;i++) cin >> a[i];
-        sort(a,a+n,cmp);
+        stable_sort(a,a+n,cmp);
         for(int i=0;i<n;i++) cout << a[i] << " ";
         cout << endl;
     }
